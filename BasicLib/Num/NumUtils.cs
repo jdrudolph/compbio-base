6258a29cf2c3bd1535253f1b3dbd9517bfd42733
unknown <cox@PCMANN160.w2k.biochem.mpg.de>
2013-01-31 13:50:28 +0100

    .

--- BasicLib/Num/NumUtils.cs
+++ BasicLib/Num/NumUtils.cs
@@ -4,7 +4,13 @@ using System.Globalization;
 using BasicLib.Util;
 
 namespace BasicLib.Num{
-	public static class NumUtils{
+	public enum CorrelationType {
+		Pearson,
+		Rsquared,
+		Spearman
+	}
+
+	public static class NumUtils {
 		public static readonly double log10 = Math.Log(10);
 
 		/// <summary>
