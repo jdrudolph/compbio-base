aa54b0d785be1e161c4520b3aaf2f9cfcefe696f
Jurgen <jurgen.cox@gmail.com>
2013-02-09 17:41:43 +0100

    .

--- BasicLib/Num/NumUtils.cs
+++ BasicLib/Num/NumUtils.cs
@@ -4,13 +4,29 @@ using System.Globalization;
 using BasicLib.Util;
 
 namespace BasicLib.Num{
-	public enum CorrelationType {
-		Pearson,
+	public enum CorrelationType{
+		AbsencePresencePval,
+		NumValidPairs,
+		ValidPairsPercentage,
+		PearsonCorrelation,
+		PearsonPval,
+		PearsonPvalPositive,
+		PearsonPvalNegative,
 		Rsquared,
-		Spearman
+		SpearmanCorrelation,
+		SpearmanPval,
+		SpearmanPvalPositive,
+		SpearmanPvalNegative,
+		KendallTau,
+		KendallPval,
+		KendallPvalPositive,
+		KendallPvalNegative,
+		DistanceCorrelation,
+		MutualInformation,
+		EuclideanDistance
 	}
 
-	public static class NumUtils {
+	public static class NumUtils{
 		public static readonly double log10 = Math.Log(10);
 
 		/// <summary>
@@ -303,12 +319,13 @@ namespace BasicLib.Num{
 			});
 		}
 
-		public static void LinFit2(double[] x, double[] y, double[] a, Action<double, double[]> funcs) {
+		public static void LinFit2(double[] x, double[] y, double[] a, Action<double, double[]> funcs){
 			double chisq;
 			LinFit2(x, y, null, a, out chisq, funcs);
 		}
 
-		public static void LinFit2(double[] x, double[] y, double[] sig, double[] a, out double chisq, Action<double, double[]> funcs) {
+		public static void LinFit2(double[] x, double[] y, double[] sig, double[] a, out double chisq,
+			Action<double, double[]> funcs){
 			double[,] covar;
 			if (sig == null){
 				sig = new double[x.Length];
@@ -1008,8 +1025,8 @@ namespace BasicLib.Num{
 		/// <summary>
 		/// Returns the error function erf(x)
 		/// </summary>
-		public static double Erff(double x) {
-			return x < 0.0 ? -Gammp(0.5, x * x) : Gammp(0.5, x * x);
+		public static double Erff(double x){
+			return x < 0.0 ? -Gammp(0.5, x*x) : Gammp(0.5, x*x);
 		}
 
 		/// <summary>
@@ -1017,21 +1034,21 @@ namespace BasicLib.Num{
 		/// </summary>
 		/// <param name="x"></param>
 		/// <returns></returns>
-		public static double Erffc(double x) {
-			return x < 0.0 ? 1.0 + Gammp(0.5, x * x) : Gammq(0.5, x * x);
+		public static double Erffc(double x){
+			return x < 0.0 ? 1.0 + Gammp(0.5, x*x) : Gammq(0.5, x*x);
 		}
 
 		/// <summary>
 		/// Returns the incomplete gamma function P(a,x)
 		/// </summary>
-		public static double Gammp(double a, double x) {
+		public static double Gammp(double a, double x){
 			double gamser = double.NaN;
 			double gammcf = double.NaN;
 			double gln;
-			if (x < 0.0 || a <= 0.0) {
+			if (x < 0.0 || a <= 0.0){
 				throw new Exception("Invalid arguments in routine gammq");
 			}
-			if (x < (a + 1.0)) {
+			if (x < (a + 1.0)){
 				Gser(ref gamser, a, x, out gln);
 				return gamser;
 			}
@@ -1042,16 +1059,16 @@ namespace BasicLib.Num{
 		/// <summary>
 		/// Returns the incomplete gamma function Q(a,x) = 1 - P(a,x)
 		/// </summary>
-		public static double Gammq(double a, double x) {
-			if (x < 0.0 || a <= 0.0) {
+		public static double Gammq(double a, double x){
+			if (x < 0.0 || a <= 0.0){
 				throw new Exception("Invalid arguments in routine gammq");
 			}
-			if (x < (a + 1.0)) {
+			if (x < (a + 1.0)){
 				double gamser = double.NaN;
 				double gln;
 				Gser(ref gamser, a, x, out gln);
 				return 1.0 - gamser;
-			} else {
+			} else{
 				double gammcf = double.NaN;
 				double gln;
 				Gcf(ref gammcf, a, x, out gln);
@@ -1065,75 +1082,75 @@ namespace BasicLib.Num{
 		private const double gserEps = 3.0e-7;
 		private const double gserItmax = 100;
 
-		public static void Gcf(ref double gammcf, double a, double x, out double gln) {
+		public static void Gcf(ref double gammcf, double a, double x, out double gln){
 			int i;
 			gln = Gammln(a);
 			double b = x + 1.0 - a;
-			double c = 1.0 / gcfFpmin;
-			double d = 1.0 / b;
+			double c = 1.0/gcfFpmin;
+			double d = 1.0/b;
 			double h = d;
-			for (i = 1; i <= gcfItmax; i++) {
-				double an = -i * (i - a);
+			for (i = 1; i <= gcfItmax; i++){
+				double an = -i*(i - a);
 				b += 2.0;
-				d = an * d + b;
-				if (Math.Abs(d) < gcfFpmin) {
+				d = an*d + b;
+				if (Math.Abs(d) < gcfFpmin){
 					d = gcfFpmin;
 				}
-				c = b + an / c;
-				if (Math.Abs(c) < gcfFpmin) {
+				c = b + an/c;
+				if (Math.Abs(c) < gcfFpmin){
 					c = gcfFpmin;
 				}
-				d = 1.0 / d;
-				double del = d * c;
+				d = 1.0/d;
+				double del = d*c;
 				h *= del;
-				if (Math.Abs(del - 1.0) < gcfEps) {
+				if (Math.Abs(del - 1.0) < gcfEps){
 					break;
 				}
 			}
-			if (i > gcfItmax) {
+			if (i > gcfItmax){
 				throw new Exception("a too large, ITMAX too small in gcf");
 			}
-			gammcf = Math.Exp(-x + a * Math.Log(x) - gln) * h;
+			gammcf = Math.Exp(-x + a*Math.Log(x) - gln)*h;
 		}
 
-		public static void Gser(ref double gamser, double a, double x, out double gln) {
+		public static void Gser(ref double gamser, double a, double x, out double gln){
 			gln = Gammln(a);
-			if (x <= 0.0) {
-				if (x < 0.0) {
+			if (x <= 0.0){
+				if (x < 0.0){
 					throw new Exception("x less than 0 in routine gser");
 				}
 				gamser = 0.0;
 				return;
 			}
 			double ap = a;
-			double sum = 1.0 / a;
+			double sum = 1.0/a;
 			double del = sum;
-			for (int n = 1; n <= gserItmax; n++) {
+			for (int n = 1; n <= gserItmax; n++){
 				++ap;
-				del *= x / ap;
+				del *= x/ap;
 				sum += del;
-				if (Math.Abs(del) < Math.Abs(sum) * gserEps) {
-					gamser = sum * Math.Exp(-x + a * Math.Log(x) - (gln));
+				if (Math.Abs(del) < Math.Abs(sum)*gserEps){
+					gamser = sum*Math.Exp(-x + a*Math.Log(x) - (gln));
 					return;
 				}
 			}
 			throw new Exception("a too large, ITMAX too small in routine gser");
 		}
 
-		public static double Bico(long n, long k) {
+		public static double Bico(long n, long k){
 			return Math.Round(Math.Exp(Factln(n) - Factln(k) - Factln(n - k)));
 		}
 
 		private static readonly double[] aaa = new double[15001];
 
-		public static double Factln(long n) {
-			if (n < 0) {
+		public static double Factln(long n){
+			if (n < 0){
 				throw new Exception("Negative factorial in routine factln");
 			}
-			if (n <= 1) {
+			if (n <= 1){
 				return 0.0;
 			}
-			if (n <= 15000) {
+			if (n <= 15000){
 				return aaa[n] != 0 ? aaa[n] : (aaa[n] = Gammln(n + 1.0));
 			}
 			return Gammln(n + 1.0);
