59d98d9142010a0af00e6468650e3ccb2cc6118e
Jurgen <jurgen.cox@gmail.com>
2013-11-25 19:25:53 +0100

    .

---
+++ BasicLib/Param/StringParam.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Windows.Controls;
+
+namespace BasicLib.Param{
+	[Serializable]
+	public class StringParam : Parameter{
+		public string Value { get; set; }
+		public string Default { get; private set; }
+		public StringParam(string name) : this(name, "") {}
+
+		public StringParam(string name, string value) : base(name){
+			Value = value;
+			Default = value;
+		}
+
+		public override string StringValue { get { return Value; } set { Value = value; } }
+		public string Value2{
+			get{
+				SetValueFromControl();
+				return Value;
+			}
+		}
+
+		public override void ResetValue(){
+			Value = Default;
+		}
+
+		public override void ResetDefault(){
+			Default = Value;
+		}
+
+		public override bool IsModified { get { return !Value.Equals(Default); } }
+
+		public override void SetValueFromControl(){
+			TextBox tb = (TextBox) control;
+			string val = tb.Text;
+			Value = val;
+		}
+
+		public override void UpdateControlFromValue(){
+			if (control == null) {
+				return;
+			}
+			TextBox tb = (TextBox)control;
+			tb.Text = Value;
+		}
+
+		public override void Clear(){
+			Value = "";
+		}
+
+		protected override Control Control { get { return new TextBox{Text = Value}; } }
+
+		public override object Clone(){
+			return new StringParam(Name, Value){Help = Help, Visible = Visible, Default = Default};
+		}
+	}
+}
