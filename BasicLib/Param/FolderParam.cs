d54bd28716b4d9cdf2000c35fe058a406716a79b
Jurgen <jurgen.cox@gmail.com>
2013-11-25 22:46:34 +0100

    .

---
+++ BasicLib/Param/FolderParam.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Controls;
+using BasicLib.Wpf;
+
+namespace BasicLib.Param{
+	[Serializable]
+	public class FolderParam : Parameter{
+		public string Value { get; set; }
+		public string Default { get; private set; }
+		public FolderParam(string name) : this(name, "") {}
+
+		public FolderParam(string name, string value) : base(name){
+			Value = value;
+			Default = value;
+		}
+
+		public override string StringValue { get { return Value; } set { Value = value; } }
+		public string Value2{
+			get{
+				SetValueFromControl();
+				return Value;
+			}
+		}
+
+		public override void ResetValue(){
+			Value = Default;
+		}
+
+		public override void ResetDefault(){
+			Default = Value;
+		}
+
+		public override bool IsModified { get { return !Value.Equals(Default); } }
+
+		public override void SetValueFromControl(){
+			FolderParameterControl tb = (FolderParameterControl)control;
+			string val = tb.Text;
+			Value = val;
+		}
+
+		public override void UpdateControlFromValue(){
+			if (control == null) {
+				return;
+			}
+			FolderParameterControl lfp = (FolderParameterControl)control;
+			lfp.Text = Value;
+		}
+
+		public override void Clear(){
+			Value = "";
+		}
+
+		protected override Control Control{
+			get{
+				FolderParameterControl tb = new FolderParameterControl { Text = Value };
+				return tb;
+			}
+		}
+
+		public override object Clone(){
+			return new FolderParam(Name, Value){Help = Help, Visible = Visible, Default = Default};
+		}
+	}
+}
---
+++ BasicLib/Wpf/FolderParameterControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BasicLib.Wpf {
+	/// <summary>
+	/// Interaction logic for FolderParameterControl.xaml
+	/// </summary>
+	public partial class FolderParameterControl : UserControl{
+		public FolderParameterControl(){
+			InitializeComponent();
+		}
+
+		public string Filter { get; set; }
+		public string Text { get; set; }
+		public bool Save { get; set; }
+		public void Connect(int connectionId, object target) {}
+	}
+}
