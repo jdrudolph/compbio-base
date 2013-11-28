28bde1f4c64c788a72f7266ad5e0699fb4e927d2
unknown <cox@PCMANN160.w2k.biochem.mpg.de>
2013-11-28 10:39:07 +0100

    .

--- BaseLib/Param/ParameterWindow.xaml.cs
+++ BaseLib/Param/ParameterWindow.xaml.cs
@@ -1,24 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace BaseLib.Param {
 	/// <summary>
 	/// Interaction logic for ParameterWindow.xaml
 	/// </summary>
 	public partial class ParameterWindow : Window {
-		public ParameterWindow() {
+		public ParameterWindow(Parameters parameters, string title, string helpDescription, string helpOutput,
+			IList<string> helpSuppls) {
 			InitializeComponent();
+			cancelButton.Click += CancelButtonClick;
+			okButton.Click += OkButtonClick;
+			parameterPanel1.Init(parameters);
+			if (!string.IsNullOrEmpty(helpDescription)) {
+				helpDoc.Blocks.Add(new Paragraph(new Run("Description:")));
+				helpDoc.Blocks.Add(new Paragraph(new Run(helpDescription)));
+			}
+			if (!string.IsNullOrEmpty(helpOutput)) {
+				helpDoc.Blocks.Add(new Paragraph(new Run("Output:")));
+				helpDoc.Blocks.Add(new Paragraph(new Run(helpOutput)));
+			}
+			if (helpSuppls != null) {
+				for (int i = 0; i < helpSuppls.Count; i++) {
+					if (!string.IsNullOrEmpty(helpSuppls[i])) {
+						helpDoc.Blocks.Add(new Paragraph(new Run("Suppl. table " + (i + 1) + ":")));
+						helpDoc.Blocks.Add(new Paragraph(new Run(helpSuppls[i])));
+					}
+				}
+			}
+			Title = title;
 		}
+
+		private void OkButtonClick(object sender, RoutedEventArgs e) {
+			Ok = true;
+			parameterPanel1.SetParameters();
+			Close();
+		}
+
+		private void CancelButtonClick(object sender, RoutedEventArgs e) {
+			Close();
+		}
+
+		public bool Ok { get; private set; }
 	}
 }
--- BaseLib/Wpf/MultiListSelectorControl.xaml.cs
+++ BaseLib/Wpf/MultiListSelectorControl.xaml.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections.Generic;
 using System.Windows.Controls;
+using BaseLib.Util;
 
 namespace BaseLib.Wpf {
 	/// <summary>
 	/// Interaction logic for MultiListSelectorControl.xaml
 	/// </summary>
 	public partial class MultiListSelectorControl : UserControl {
+		private MultiListSelectorSubSelectionControl[] subSelection;
+		public IList<string> items;
+
 		public MultiListSelectorControl() {
 			InitializeComponent();
 		}
+
+		public void Init(IList<string> items1) {
+			//TODO
+		}
+
 
 		public int[][] SelectedIndices { get; set; }
 
@@ -19,5 +28,19 @@ namespace BaseLib.Wpf {
 		public void Init(IList<string> values, IList<string> bins){
 			
 		}
+
+		public void SetSelected(int selectorInd, int itemInd, bool b) {
+			//TODO
+		}
+
+		public int[] GetSelectedIndices(int selectorInd) {
+			string[] sel = subSelection[selectorInd].SelectedStrings;
+			int[] result = new int[sel.Length];
+			for (int i = 0; i < result.Length; i++) {
+				result[i] = ArrayUtils.IndexOf(items, sel[i]);
+			}
+			return result;
+		}
+
 	}
 }
