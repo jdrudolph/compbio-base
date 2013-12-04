b3d925712f75e7d128658a42dccb1101eab5b2e6
unknown <cox@PCMANN160.w2k.biochem.mpg.de>
2013-12-04 09:48:21 +0100

    .

--- BaseLib/Wpf/WpfUtils.cs
+++ BaseLib/Wpf/WpfUtils.cs
@@ -1,11 +1,19 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace BaseLib.Wpf {
 	public static class WpfUtils {
 		public static UIElement GetGridChild(Grid grid, int row, int column) {
 			return grid.Children.Cast<UIElement>().First(e => Grid.GetRow(e) == row && Grid.GetColumn(e) == column);
+		}
+
+		public static ImageSource LoadBitmap(System.Drawing.Bitmap source) {
+			return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(source.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty,
+				BitmapSizeOptions.FromEmptyOptions());
 		}
 	}
 }
