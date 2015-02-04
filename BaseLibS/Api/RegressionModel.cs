153121b54e70c7f78e9bda901d8e129299ce59ac
Jurgen <jurgen.cox@gmail.com>
2015-02-04 16:46:36 +0100

    .

--- BaseLibS/Api/RegressionModel.cs
+++ BaseLibS/Api/RegressionModel.cs
@@ -1,8 +1,25 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace BaseLibS.Api{
 	[Serializable]
 	public abstract class RegressionModel{
 		public abstract float Predict(BaseVector x);
+
+		public void Write(string filePath){
+			Stream stream = File.Open(filePath, FileMode.Create);
+			BinaryFormatter bFormatter = new BinaryFormatter();
+			bFormatter.Serialize(stream, this);
+			stream.Close();
+		}
+
+		public static RegressionModel Read(string filePath){
+			Stream stream = File.Open(filePath, FileMode.Open);
+			BinaryFormatter bFormatter = new BinaryFormatter();
+			RegressionModel m = (RegressionModel) bFormatter.Deserialize(stream);
+			stream.Close();
+			return m;
+		}
 	}
 }
