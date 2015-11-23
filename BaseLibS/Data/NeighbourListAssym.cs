1ea602c0dd4e1b233c9d4a9e4f9fe14c42cd3ab7
JurgenCox <cox@biochem.mpg.de>
2015-11-23 07:55:31 +0100

    .

--- BaseLibS/Data/NeighbourListAssym.cs
+++ BaseLibS/Data/NeighbourListAssym.cs
@@ -18,50 +18,65 @@ namespace BaseLibS.Data{
 			neighborList2[j].Add(i);
 		}
 
-		//public bool IsEmpty1At(int i){
-		//	return !neighborList1.ContainsKey(i);
-		//}
+		public void GetClusterAt(int i, out int[] cluster1, out int[] cluster2){
+			GetClusterAtNoRemove(i, out cluster1, out cluster2);
+			RemoveCluster(cluster1, cluster2);
+		}
 
-		//public bool IsEmpty2At(int j){
-		//	return !neighborList2.ContainsKey(j);
-		//}
+		public void GetAllClusters(out int[][] clusters1, out int[][] clusters2){
+			List<int[]> result1 = new List<int[]>();
+			List<int[]> result2 = new List<int[]>();
+			while (neighborList1.Count > 0){
+				int[] cluster1;
+				int[] cluster2;
+				GetClusterAt(neighborList1.Keys.First(), out cluster1, out cluster2);
+				result1.Add(cluster1);
+				result2.Add(cluster2);
+			}
+			clusters1 = result1.ToArray();
+			clusters2 = result2.ToArray();
+		}
 
-		//public int[] GetCluster1At(int i){
-		//	int[] cluster = GetCluster1AtNoRemove(i);
-		//	RemoveCluster(cluster);
-		//	return cluster;
-		//}
+		public void RemoveCluster(int[] cluster1, int[] cluster2){
+			foreach (int c in cluster1.Where(c => neighborList1.ContainsKey(c))){
+				neighborList1.Remove(c);
+			}
+			foreach (int c in cluster2.Where(c => neighborList2.ContainsKey(c))){
+				neighborList2.Remove(c);
+			}
+		}
 
-		//public int[][] GetAllClusters(){
-		//	List<int[]> result = new List<int[]>();
-		//	while (neighborList.Count > 0){
-		//		result.Add(GetClusterAt(neighborList.Keys.First()));
-		//	}
-		//	return result.ToArray();
-		//}
-
-		//public void RemoveCluster(int[] cluster){
-		//	foreach (int c in cluster.Where(c => neighborList.ContainsKey(c))){
-		//		neighborList.Remove(c);
-		//	}
-		//}
-
-		//public int[] GetClusterAtNoRemove(int i){
-		//	HashSet<int> cluster = new HashSet<int>();
-		//	Stack<int> todo = new Stack<int>();
-		//	todo.Push(i);
-		//	while (todo.Count > 0){
-		//		int next = todo.Pop();
-		//		if (!cluster.Contains(next)){
-		//			if (neighborList.ContainsKey(next)){
-		//				cluster.Add(next);
-		//				foreach (int x in neighborList[next]){
-		//					todo.Push(x);
-		//				}
-		//			}
-		//		}
-		//	}
-		//	return ArrayUtils.ToArray(cluster);
-		//}
+		public void GetClusterAtNoRemove(int i, out int[] c1, out int[] c2){
+			HashSet<int> cluster1 = new HashSet<int>();
+			HashSet<int> cluster2 = new HashSet<int>();
+			Stack<int> todo1 = new Stack<int>();
+			Stack<int> todo2 = new Stack<int>();
+			todo1.Push(i);
+			while (todo1.Count > 0 || todo2.Count > 0){
+				if (todo1.Count > 0){
+					int next = todo1.Pop();
+					if (!cluster1.Contains(next)){
+						if (neighborList1.ContainsKey(next)){
+							cluster1.Add(next);
+							foreach (int x in neighborList1[next]){
+								todo2.Push(x);
+							}
+						}
+					}
+				} else{
+					int next = todo2.Pop();
+					if (!cluster2.Contains(next)){
+						if (neighborList2.ContainsKey(next)){
+							cluster2.Add(next);
+							foreach (int x in neighborList2[next]){
+								todo1.Push(x);
+							}
+						}
+					}
+				}
+			}
+			c1 = ArrayUtils.ToArray(cluster1);
+			c2 = ArrayUtils.ToArray(cluster2);
+		}
 	}
 }
