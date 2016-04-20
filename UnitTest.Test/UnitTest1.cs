027690e60d1fcd1f43c9b6c7eb5e010279a6255d
Jan Rudolph <rudolph@biochem.mpg.de>
2016-04-20 14:27:54 +0200

    Add simple unit-test project

---
+++ UnitTest.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BaseLibS.Param;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var f = new FileParam("Just a test");
+            Assert.IsNotNull(f);
+        }
+    }
+}
