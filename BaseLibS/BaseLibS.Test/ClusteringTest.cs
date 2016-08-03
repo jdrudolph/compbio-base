89241f98cdc9f94b9ed5efe69a496d8c96280f0f
Jan Rudolph <rudolph@biochem.mpg.de>
2016-08-03 10:20:26 +0200

    kmeans bug

---
+++ BaseLibS/BaseLibS.Test/ClusteringTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using BaseLibS.Api;
+using BaseLibS.Num.Cluster;
+using BaseLibS.Num.Matrix;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumPluginBase.Distance;
+
+namespace BaseLibS.Test
+{
+    [TestClass]
+    public class ClusteringTest
+    {
+        [TestMethod]
+        [DeploymentItem(@"Examples", "Examples")]
+        public void TestClustering()
+        {
+            var hclust = new HierarchicalClustering();
+            var vals = Util.ReadMatrix("Examples/clustering_array_copy_error.txt.gz");
+            var data = new FloatMatrixIndexer(vals);
+            var distance = new EuclideanDistance();
+            hclust.TreeClusterKmeans(data, MatrixAccess.Columns, distance, HierarchicalClusterLinkage.Average, false,
+                false, 1, 300, 1, 100,
+                (i) => { });
+        }
+    }
+}
