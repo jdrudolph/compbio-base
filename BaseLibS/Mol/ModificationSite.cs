6735ac28755e01c55a99cc7fe1404dc9e395489a
Juergen Cox <cox@biochem.mpg.de>
2016-08-22 16:41:57 +0200

    reverting ModificationSite

--- BaseLibS/Mol/ModificationSite.cs
+++ BaseLibS/Mol/ModificationSite.cs
@@ -3,14 +3,22 @@ using System.Xml.Serialization;
 namespace BaseLibS.Mol{
 	public class ModificationSite{
 		[XmlAttribute("site")]
-		public string Site { get { return "" + Aa; } set { Aa = value[0]; } }
+		public string Site{
+			get { return "" + Aa; }
+			set { Aa = value[0]; }
+		}
+
 		[XmlIgnore]
 		public char Aa { get; set; }
-        [XmlArrayItem("neutralloss")]
-		public NeutralLoss[] NeutrallossCollection { get; set; }
-        [XmlArrayItem("diagnostic")]
-		public DiagnosticPeak[] DiagnosticCollection { get; set; }
-		public bool HasNeutralLoss => NeutrallossCollection != null && NeutrallossCollection.Length != 0;
-		public bool HasDiagnosticPeak => DiagnosticCollection != null && DiagnosticCollection.Length != 0;
+
+		[XmlArrayItem("neutralloss")]
+		public NeutralLoss[] neutralloss_collection { get; set; }
+
+		[XmlArrayItem("diagnostic")]
+		public DiagnosticPeak[] diagnostic_collection { get; set; }
+
+		public bool HasNeutralLoss => neutralloss_collection != null && neutralloss_collection.Length != 0;
+
+		public bool HasDiagnosticPeak => diagnostic_collection != null && diagnostic_collection.Length != 0;
 	}
 }
