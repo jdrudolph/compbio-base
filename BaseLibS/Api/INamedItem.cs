62b31e778e2baac59ca5b20511a4f2c0fca77cc3
JurgenCox <cox@biochem.mpg.de>
2016-08-31 22:18:20 +0200

    minor

--- BaseLibS/Api/INamedItem.cs
+++ BaseLibS/Api/INamedItem.cs
@@ -1,13 +1,13 @@
 ﻿namespace BaseLibS.Api{
-    public interface INamedItem{
-        /// <summary>
-        /// This is the name that e.g. appears in drop-down menus.
-        /// </summary>
-        string Name { get; }
+	public interface INamedItem{
+		/// <summary>
+		/// This is the name that e.g. appears in drop-down menus.
+		/// </summary>
+		string Name { get; }
 
-        /// <summary>
-        /// The context help that will appear in tool tips etc. 
-        /// </summary>
-        string Description { get; }
-    }
+		/// <summary>
+		/// The context help that will appear in tool tips etc. 
+		/// </summary>
+		string Description { get; }
+	}
 }
