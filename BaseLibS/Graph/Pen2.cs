b549247c0032e950541d392181bb023f2ff01845
JurgenCox <cox@biochem.mpg.de>
2016-09-01 09:32:54 +0200

    Making Pen2 serializable

--- BaseLibS/Graph/Pen2.cs
+++ BaseLibS/Graph/Pen2.cs
@@ -1,4 +1,7 @@
-﻿namespace BaseLibS.Graph{
+﻿using System;
+
+namespace BaseLibS.Graph{
+	[Serializable]
 	public class Pen2{
 		public Pen2(Color2 color) : this(color, 1f){}
 
@@ -12,31 +15,18 @@
 		public float Width { get; set; }
 		public DashCap2 DashCap { get; set; }
 		public DashStyle2 DashStyle { get; set; }
-		//
-		// Summary:
-		//     Gets or sets the distance from the start of a line to the beginning of a dash
-		//     pattern.
-		//
-		// Returns:
-		//     The distance from the start of a line to the beginning of a dash pattern.
-		//
-		// Exceptions:
-		//   T:System.ArgumentException:
-		//     The System.Drawing.Pen.DashOffset property is set on an immutable System.Drawing.Pen,
-		//     such as those returned by the System.Drawing.Pens class.
+
+		/// <summary>
+		/// Gets or sets the distance from the start of a line to the beginning of a dash pattern.
+		/// Returns the distance from the start of a line to the beginning of a dash pattern. 
+		/// </summary>
 		public float DashOffset { get; set; }
-		//
-		// Summary:
-		//     Gets or sets an array of custom dashes and spaces.
-		//
-		// Returns:
-		//     An array of real numbers that specifies the lengths of alternating dashes and
-		//     spaces in dashed lines.
-		//
-		// Exceptions:
-		//   T:System.ArgumentException:
-		//     The System.Drawing.Pen.DashPattern property is set on an immutable System.Drawing.Pen,
-		//     such as those returned by the System.Drawing.Pens class.
+
+		/// <summary>
+		/// Gets or sets an array of custom dashes and spaces.
+		/// Returns an array of real numbers that specifies the lengths of alternating dashes and 
+		/// spaces in dashed lines. 
+		/// </summary>
 		public float[] DashPattern { get; set; }
 	}
 }
