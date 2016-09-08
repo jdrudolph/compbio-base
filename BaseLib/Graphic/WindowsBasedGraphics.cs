6ae3cfae091924c03e346fc941286d8cbf2b2ba0
Stefka Tyanova <tyanova@biochem.mpg.de>
2016-09-08 11:58:21 +0200

    Fixed arrow heads

--- BaseLib/Graphic/WindowsBasedGraphics.cs
+++ BaseLib/Graphic/WindowsBasedGraphics.cs
@@ -82,34 +82,49 @@ namespace BaseLib.Graphic{
 			float newY2;
 			float x3, x4, y3, y4;
 			if (y1.Equals(y2)){
-				newX2 = x2 - offset;
-				newY2 = y2;
-				x3 = newX2;
-				x4 = newX2;
-				y3 = newY2 - side/2;
-				y4 = newY2 + side/2;
+			    if (x1 < x2) 
+			        newX2 = x2 - offset;
+                else
+                    newX2 = x2 + offset;
+                newY2 = y2;
+			    x3 = newX2;
+			    x4 = newX2;
+			    y3 = newY2 - side/2;
+			    y4 = newY2 + side/2;
+                gc.DrawLine(GetPen(pen), x1, y1, newX2, newY2);
 			} else if (x1.Equals(x2)){
-				newX2 = x2;
-				newY2 = y2 - offset;
-				y3 = newY2;
-				y4 = newY2;
-				x3 = newX2 - side/2;
-				x4 = newX2 + side/2;
-			} else{
-				float m = (y2 - y1)/(x2 - x1);
+			    if (y1 < y2) 
+			        newY2 = y2 - offset;
+                else
+                    newY2 = y2 + offset;
+                newX2 = x2;
+                y3 = newY2;
+			    y4 = newY2;
+			    x3 = newX2 - side/2;
+			    x4 = newX2 + side/2;
+                gc.DrawLine(GetPen(pen), x1, y1, newX2, newY2);
+			}
+            else {  
+			    float m = (y2 - y1)/(x2 - x1);
 				float m2 = m*m;
 				float sq = (float) Math.Sqrt(1 + m2);
-				newX2 = x2 - offset/sq;
-				newY2 = y2 - m*offset/sq;
-				float n = -1/((y2 - y1)/(x2 - x1));
-				float n2 = n*n;
-				float sqn = (float) Math.Sqrt(1 + n2);
-				x3 = newX2 + (side/2)/sqn;
-				y3 = newY2 + n*(side/2)/sqn;
-				x4 = newX2 - (side/2)/sqn;
-				y4 = newY2 - n*(side/2)/sqn;
+                float n = -1 / ((y2 - y1) / (x2 - x1));
+                float n2 = n * n;
+                float sqn = (float)Math.Sqrt(1 + n2);
+			    if (x2 > x1) {
+			        newX2 = x2 - offset/sq;
+			        newY2 = y2 - m*offset/sq;
+			    }
+			    else {
+                    newX2 = x2 + offset / sq;
+                    newY2 = y2 + m * offset / sq;
+                }
+			    x3 = newX2 + (side/2)/sqn;
+			    y3 = newY2 + n*(side/2)/sqn;
+			    x4 = newX2 - (side/2)/sqn;
+			    y4 = newY2 - n*(side/2)/sqn;
+			    gc.DrawLine(GetPen(pen), x1, y1, newX2, newY2);
 			}
-			gc.DrawLine(GetPen(pen), x1, y1, newX2, newY2);
 			gc.DrawPolygon(GetPen(pen),
 				new[]{
 					new Point(Convert.ToInt32(x2), Convert.ToInt32(y2)), new Point(Convert.ToInt32(x3), Convert.ToInt32(y3)),
