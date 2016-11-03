8012e9a264999fa6129b21ee916c76784470b588
Jan Rudolph <rudolph@biochem.mpg.de>
2016-11-03 16:40:44 +0100

    port regex replace param to winforms

---
+++ BaseLib/Forms/RegexReplaceParamControl.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BaseLib.Wpf;
+using System.Text.RegularExpressions;
+
+namespace BaseLib.Forms
+{
+    public partial class RegexReplaceParamControl : UserControl
+    {
+        private Regex _pattern;
+        private string _replacement;
+        private List<string> _previews;
+
+        public RegexReplaceParamControl(Regex pattern, string replacement, List<string> previews)
+        {
+            InitializeComponent();
+            _pattern = pattern;
+            _replacement = replacement;
+            _previews = previews;
+
+            PatternTextBox.Text = Pattern;
+            PatternTextBox.TextChanged += (sender, args) =>
+            {
+                Pattern = PatternTextBox.Text;
+            };
+            ReplaceTextBox.Text = Replacement;
+            ReplaceTextBox.TextChanged += (sender, args) =>
+            {
+                Replacement = ReplaceTextBox.Text;
+            };
+
+            CurrentListBox.Items.Clear();
+            CurrentListBox.Items.AddRange(Current);
+
+            UpdatePreview();
+        }
+
+        private string[] Current => _previews.ToArray();
+        private string[] Preview => _previews.Select(str => _pattern.Replace(str, _replacement)).ToArray();
+
+        public string Replacement
+        {
+            get { return _replacement; }
+            set
+            {
+                if (_replacement != value)
+                {
+                    _replacement = value;
+                    UpdatePreview();
+                }
+            }
+        }
+
+        public string Pattern
+        {
+            get { return _pattern.ToString(); }
+            set
+            {
+                try
+                {
+                    var pattern = new Regex(value);
+                    if (_pattern != pattern)
+                    {
+                        _pattern = pattern;
+                        UpdatePreview();
+                    }
+                }
+                catch (ArgumentException)
+                {
+                    Debug.WriteLine("Illegal regex");
+                }
+            }
+        }
+
+        private void UpdatePreview()
+        {
+            PreviewListBox.Items.Clear();
+            PreviewListBox.Items.AddRange(Preview);
+        }
+
+        public Tuple<Regex, string> GetValue() => Tuple.Create(_pattern, _replacement);
+    }
+}
