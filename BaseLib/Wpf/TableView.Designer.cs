76ec92b3452ba02d8b4039885f4d4d057a85d1e0
JurgenCox <cox@biochem.mpg.de>
2016-11-10 19:10:13 +0100

    table in winforms

--- BaseLib/Wpf/TableView.Designer.cs
+++ BaseLib/Wpf/TableView.Designer.cs
@@ -28,27 +28,91 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.selectionAgentButton = new System.Windows.Forms.Button();
+			this.textButton = new System.Windows.Forms.Button();
+			this.itemsLabel = new System.Windows.Forms.Label();
+			this.selectedLabel = new System.Windows.Forms.Label();
 			this.mainPanel = new System.Windows.Forms.Panel();
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-			this.splitContainer1.Panel1.SuspendLayout();
-			this.splitContainer1.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// splitContainer1
+			// tableLayoutPanel1
 			// 
-			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-			this.splitContainer1.Margin = new System.Windows.Forms.Padding(0);
-			this.splitContainer1.Name = "splitContainer1";
-			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.mainPanel, 0, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 18F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(523, 538);
+			this.tableLayoutPanel1.TabIndex = 0;
 			// 
-			// splitContainer1.Panel1
+			// tableLayoutPanel2
 			// 
-			this.splitContainer1.Panel1.Controls.Add(this.mainPanel);
-			this.splitContainer1.Size = new System.Drawing.Size(523, 538);
-			this.splitContainer1.SplitterDistance = 421;
-			this.splitContainer1.TabIndex = 0;
+			this.tableLayoutPanel2.ColumnCount = 5;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 18F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 18F));
+			this.tableLayoutPanel2.Controls.Add(this.selectionAgentButton, 3, 0);
+			this.tableLayoutPanel2.Controls.Add(this.textButton, 4, 0);
+			this.tableLayoutPanel2.Controls.Add(this.itemsLabel, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.selectedLabel, 1, 0);
+			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 520);
+			this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 1;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(523, 18);
+			this.tableLayoutPanel2.TabIndex = 0;
+			// 
+			// selectionAgentButton
+			// 
+			this.selectionAgentButton.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.selectionAgentButton.Location = new System.Drawing.Point(487, 0);
+			this.selectionAgentButton.Margin = new System.Windows.Forms.Padding(0);
+			this.selectionAgentButton.Name = "selectionAgentButton";
+			this.selectionAgentButton.Size = new System.Drawing.Size(18, 18);
+			this.selectionAgentButton.TabIndex = 0;
+			this.selectionAgentButton.UseVisualStyleBackColor = true;
+			// 
+			// textButton
+			// 
+			this.textButton.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textButton.Location = new System.Drawing.Point(505, 0);
+			this.textButton.Margin = new System.Windows.Forms.Padding(0);
+			this.textButton.Name = "textButton";
+			this.textButton.Size = new System.Drawing.Size(18, 18);
+			this.textButton.TabIndex = 1;
+			this.textButton.UseVisualStyleBackColor = true;
+			// 
+			// itemsLabel
+			// 
+			this.itemsLabel.AutoSize = true;
+			this.itemsLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.itemsLabel.Location = new System.Drawing.Point(3, 0);
+			this.itemsLabel.Name = "itemsLabel";
+			this.itemsLabel.Size = new System.Drawing.Size(1, 18);
+			this.itemsLabel.TabIndex = 2;
+			// 
+			// selectedLabel
+			// 
+			this.selectedLabel.AutoSize = true;
+			this.selectedLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.selectedLabel.Location = new System.Drawing.Point(9, 0);
+			this.selectedLabel.Name = "selectedLabel";
+			this.selectedLabel.Size = new System.Drawing.Size(1, 18);
+			this.selectedLabel.TabIndex = 3;
 			// 
 			// mainPanel
 			// 
@@ -56,26 +120,31 @@
 			this.mainPanel.Location = new System.Drawing.Point(0, 0);
 			this.mainPanel.Margin = new System.Windows.Forms.Padding(0);
 			this.mainPanel.Name = "mainPanel";
-			this.mainPanel.Size = new System.Drawing.Size(523, 421);
-			this.mainPanel.TabIndex = 0;
+			this.mainPanel.Size = new System.Drawing.Size(523, 520);
+			this.mainPanel.TabIndex = 1;
 			// 
 			// TableView
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.tableLayoutPanel1);
 			this.Name = "TableView";
 			this.Size = new System.Drawing.Size(523, 538);
-			this.splitContainer1.Panel1.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-			this.splitContainer1.ResumeLayout(false);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.tableLayoutPanel2.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.Button selectionAgentButton;
+		private System.Windows.Forms.Button textButton;
+		private System.Windows.Forms.Label itemsLabel;
+		private System.Windows.Forms.Label selectedLabel;
 		private System.Windows.Forms.Panel mainPanel;
 	}
 }
--- BaseLib/Wpf/TableView.cs
+++ BaseLib/Wpf/TableView.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 using BaseLib.Forms.Scroll;
 using BaseLib.Forms.Table;
+using BaseLibS.Num;
+using BaseLibS.Table;
+using BaseLibS.Util;
 
 namespace BaseLib.Wpf{
 	public partial class TableView : UserControl{
@@ -15,9 +19,264 @@ namespace BaseLib.Wpf{
 		private ITableSelectionAgent selectionAgent;
 		private int selectionAgentColInd = -1;
 		private double[] selectionAgentColVals;
+		private readonly TextBox auxTextBox;
 
 		public TableView(){
 			InitializeComponent();
+			tableView = new CompoundScrollableControl();
+			tableViewWf = new TableViewControlModel();
+			tableView.Client = tableViewWf;
+			tableViewWf.SelectionChanged += (sender, args) =>{
+				SelectionChanged?.Invoke(sender, args);
+				long c = tableViewWf.SelectedCount;
+				long t = tableViewWf.RowCount;
+				selectedLabel.Text = c > 0 && MultiSelect ? "" + StringUtils.WithDecimalSeparators(c) + " selected" : "";
+				itemsLabel.Text = "" + StringUtils.WithDecimalSeparators(t) + " item" + (t == 1 ? "" : "s");
+			};
+			mainPanel.Controls.Add(tableView);
+			textButton.Click += TextButton_OnClick;
+			selectionAgentButton.Click += SelectionAgentButton_OnClick;
+			KeyDown += (sender, args) => tableView.Focus();
+			auxTextBox = new TextBox{Dock = DockStyle.Fill, Padding = new Padding(0)};
+		}
+
+		public void SelectTime(double timeMs){
+			if (selectionAgentColInd < 0){
+				return;
+			}
+			int ind = ArrayUtils.ClosestIndex(selectionAgentColVals, timeMs);
+			ClearSelection();
+			SetSelectedIndex(ind);
+		}
+
+		public static void RegisterSelectionAgent(ITableSelectionAgent agent){
+			selectionAgents.Add(agent);
+		}
+
+		public static void UnregisterSelectionAgent(ITableSelectionAgent agent){
+			selectionAgents.Remove(agent);
+		}
+
+		public bool HasSelectionAgent{
+			get { return hasSelectionAgent; }
+			set{
+				hasSelectionAgent = value;
+				if (hasSelectionAgent && selectionAgents.Count > 0){
+					selectionAgentButton.Visible = true;
+				}
+			}
+		}
+
+		//public static readonly DependencyProperty TableModelProperty = DependencyProperty.Register("TableModel",
+		//	typeof (ITableModel), typeof (TableViewWpf), new PropertyMetadata(default(ITableModel), (o, args) =>{
+		//		var x = (TableViewWpf) o;
+		//		var value = (ITableModel) args.NewValue;
+		//		x.tableViewWf.TableModel = value;
+		//		x.ItemsTextBlock.Text = value != null ? "" + StringUtils.WithDecimalSeparators(value.RowCount) + " items" : "";
+		//	}));
+
+		/// <summary>
+		/// Get the table model.
+		/// Use <code>Dispatcher.Invoke(() => view.TableModel ... )</code> to access this property for a non-GUI thread
+		/// </summary>
+		public ITableModel TableModel{
+			get { return tableViewWf.TableModel; }
+			set{
+				tableViewWf.TableModel = value;
+				itemsLabel.Text = value != null ? "" + StringUtils.WithDecimalSeparators(value.RowCount) + " items" : "";
+			}
+		}
+
+		//public void Select(){
+		//	tableView.Select();
+		//}
+		SplitContainer splitContainer;
+		public void SwitchOnTextBox(){
+			tableViewWf.SetCellText = s => auxTextBox.Text = s;
+			mainPanel.Controls.Remove(tableView);
+			splitContainer = new SplitContainer();
+			splitContainer.Panel1.Controls.Add(tableView);
+			splitContainer.Panel2.Controls.Add(auxTextBox);
+			mainPanel.Controls.Add(splitContainer);
+		}
+
+		public void SwitchOffTextBox(){
+			auxTextBox.Text = "";
+			tableViewWf.SetCellText = null;
+			mainPanel.Controls.Remove(splitContainer);
+			splitContainer.Panel1.Controls.Remove(tableView);
+			splitContainer.Panel2.Controls.Remove(auxTextBox);
+			splitContainer = null;
+			mainPanel.Controls.Add(tableView);
+		}
+
+		public bool MultiSelect{
+			get { return tableViewWf.MultiSelect; }
+			set { tableViewWf.MultiSelect = value; }
+		}
+
+		public bool Sortable{
+			get { return tableViewWf.Sortable; }
+			set { tableViewWf.Sortable = value; }
+		}
+
+		public int RowCount => tableViewWf.RowCount;
+
+		public int RowHeaderWidth{
+			get { return tableView.RowHeaderWidth; }
+			set { tableView.RowHeaderWidth = value; }
+		}
+
+		public int ColumnHeaderHeight{
+			get { return tableView.ColumnHeaderHeight; }
+			set{
+				tableViewWf.origColumnHeaderHeight = value;
+				tableView.ColumnHeaderHeight = value;
+			}
+		}
+
+		public int VisibleX{
+			get { return tableView.VisibleX; }
+			set { tableView.VisibleX = value; }
+		}
+
+		public int VisibleY{
+			get { return tableView.VisibleY; }
+			set { tableView.VisibleY = value; }
+		}
+
+		public void SetSelectedRow(int row){
+			tableViewWf.SetSelectedRow(row);
+		}
+
+		public void SetSelectedRow(int row, bool add, bool fire){
+			tableViewWf.SetSelectedRow(row, add, fire);
+		}
+
+		public bool HasSelectedRows(){
+			return tableViewWf.HasSelectedRows();
+		}
+
+		public void SetSelectedRows(IList<int> rows){
+			tableViewWf.SetSelectedRows(rows);
+		}
+
+		public void SetSelectedRows(IList<int> rows, bool add, bool fire){
+			tableViewWf.SetSelectedRows(rows, add, fire);
+		}
+
+		public void SetSelectedRowAndMove(int row){
+			tableViewWf.SetSelectedRowAndMove(row);
+		}
+
+		public void SetSelectedRowsAndMove(IList<int> rows){
+			tableViewWf.SetSelectedRowsAndMove(rows);
+		}
+
+		public int[] GetSelectedRows(){
+			return tableViewWf.GetSelectedRows();
+		}
+
+		public int GetSelectedRow(){
+			return tableViewWf.GetSelectedRow();
+		}
+
+		public void ScrollToRow(int row){
+			tableViewWf.ScrollToRow(row);
+		}
+
+		public void BringSelectionToTop(){
+			tableViewWf.BringSelectionToTop();
+		}
+
+		public void FireSelectionChange(){
+			tableViewWf.FireSelectionChange();
+		}
+
+		public bool ModelRowIsSelected(int row){
+			return tableViewWf.ModelRowIsSelected(row);
+		}
+
+		public void ClearSelection(){
+			tableViewWf.ClearSelection();
+		}
+
+		public void SelectAll(){
+			tableViewWf.SelectAll();
+		}
+
+		public void SetSelection(bool[] selection){
+			tableViewWf.SetSelection(selection);
+		}
+
+		public void SetSelectedIndex(int index){
+			tableViewWf.SetSelectedIndex(index);
+		}
+
+		public void SetSelectedViewIndex(int index){
+			tableViewWf.SetSelectedViewIndex(index);
+		}
+
+		public void SetSelectedIndex(int index, object sender){
+			tableViewWf.SetSelectedIndex(index, sender);
+		}
+
+		public object GetEntry(int row, int col){
+			return tableViewWf.GetEntry(row, col);
+		}
+
+		private void TextButton_OnClick(object sender, EventArgs e){
+			if (textBoxVisible){
+				SwitchOffTextBox();
+			} else{
+				SwitchOnTextBox();
+			}
+			textBoxVisible = !textBoxVisible;
+		}
+
+		//public void RegisterScrollViewer(ScrollViewer scrollViewer){
+		//	MainPanel.RegisterScrollViewer(scrollViewer);
+		//}
+
+		//public void UnregisterScrollViewer(ScrollViewer scrollViewer){
+		//	MainPanel.UnregisterScrollViewer(scrollViewer);
+		//}
+
+		public void ClearSelectionFire(){
+			tableViewWf.ClearSelectionFire();
+		}
+
+		private void SelectionAgentButton_OnClick(object sender, EventArgs e){
+			Point p = selectionAgentButton.PointToScreen(new Point(0, 0));
+			TableViewSelectionAgentForm w = new TableViewSelectionAgentForm(TableModel){
+				Top = p.Y - 125,
+				Left = p.X - 300
+			};
+			if (w.ShowDialog() == DialogResult.OK){
+				int ind1 = w.sourceBox.SelectedIndex;
+				int ind2 = w.columnBox.SelectedIndex;
+				if (ind1 >= 0 && ind2 >= 0){
+					selectionAgent = selectionAgents[ind1];
+					selectionAgentColInd = ind2;
+					selectionAgentColVals = GetTimeVals(ind2);
+					//TODO
+					//selectionAgent.AddTable(this);
+				} else{
+					selectionAgent = null;
+					selectionAgentColInd = -1;
+					selectionAgentColVals = null;
+					//TODO
+					//selectionAgent.RemoveTable(this);
+				}
+			}
+		}
+
+		private double[] GetTimeVals(int ind2){
+			double[] result = new double[TableModel.RowCount];
+			for (int i = 0; i < result.Length; i++){
+				result[i] = (double) TableModel.GetEntry(i, ind2);
+			}
+			return result;
 		}
 	}
 }
